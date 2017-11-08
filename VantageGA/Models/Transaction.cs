c07f2f1bf1bffd855daa08bd3d4b86fa0ca661b7
sonipuneet <puneetsoni.mail@gmail.com>
2017-11-09 00:09:16 +0530

    Added UI

--- VantageGA/Models/Transaction.cs
+++ VantageGA/Models/Transaction.cs
@@ -8,11 +8,18 @@ namespace VantageGA.Models
 {
     public class Transaction
     {
-        public string TransactionType { get; set; }
+        public string TransactionType{ get; set; }
         
         public string InvoiceNumber { get; set; }
         [Required(ErrorMessage = "Please provide the Invoice Number", AllowEmptyStrings = false)]
         public string TransactionTotal { get; set; }
         [Required(ErrorMessage = "Please provide the Transaction Total", AllowEmptyStrings = false)]
+    }
+
+    public enum TransactionType
+    {
+        PrePay,
+        PostPay,
+        PayAtThePump,LoyaltyOutPayIn(LOPI)
     }
 }
