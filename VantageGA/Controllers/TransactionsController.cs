7fbb4820987185ba43f8a51686a5bd3055cc946d
sonipuneet <puneetsoni.mail@gmail.com>
2017-11-08 22:26:33 +0530

    Added Site.CSS

---
+++ VantageGA/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VantageGA.Models;
+
+namespace VantageGA.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        // GET: Transaction/TransactionDetails
+        public ActionResult TransactionDetails()
+        {
+
+            var TransactionDetails = new Transaction();
+            return View(TransactionDetails);
+        }
+
+        // GET: Transaction/Details
+
+        public ActionResult TransactionInfo()
+        {
+
+            var TransactionInfo = new Transaction();
+            return View(TransactionInfo);
+        }
+    }
+}
