95c2e393afdeffbb2442f776a13044df7c5023f4
sonipuneet <puneetsoni.mail@gmail.com>
2017-11-14 21:51:40 +0530

    Modified TransactionDetails and INFO

--- VantageGA/Models/Transaction.cs
+++ VantageGA/Models/Transaction.cs
@@ -9,7 +9,8 @@ namespace VantageGA.Models
     public class Transaction
     {
         public string TransactionType{ get; set; }
-        
+        [Required(ErrorMessage = "Transaction Type is Required.")]
+
         public string InvoiceNumber { get; set; }
         [Required(ErrorMessage = "Please provide the Invoice Number", AllowEmptyStrings = false)]
         public string TransactionTotal { get; set; }
